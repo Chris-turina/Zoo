ea304a882acee840ea66d72a39851326c9ae5454
chris turina <chris.turina@gmail.com>
2017-12-08 09:04:18 -0800

    displays a list

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Animal } from './animal.model';
 
 @Component({
   selector:'app-root',
-  templateUrl:''
+  templateUrl: 'app/app.component.html',
 })
 
 export class AppComponent {
-  
+  public species: string;
+  public name: string;
+  public age: number;
+  public diet: string;
+  public zooLocation: string;
+  public cartakers: number;
+  public sex: string;
+  public like: string;
+  public dislike: string;
+
+  public masterAnimalList: Animal[] = [
+    new Animal('Artic Fox', 'Moon', 2, 'Carnivore', 'Norther Trail', 5, 'Female', 'Cool Shade', 'Loud Noises')
+  ]
 }
--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent }   from './app.component';
+import { AnimalListComponent } from './animal-list.component';
 
 @NgModule({
-  imports: [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports: [ BrowserModule, ],
+  declarations: [ AppComponent,
+                  AnimalListComponent],
   bootstrap: [ AppComponent ]
 })
 
 export class AppModule {
-  
+
 }
